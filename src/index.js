a0b74555a7248a3b47e5700eae010ecfd57c4b41
Edmund <edmundgachanja@gmail.com>
2024-12-14 17:39:00 +0300

    Finsihes lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.querySelector('form');
+
+  newTaskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const toDo = e.target.querySelector('#new-task-description').value 
+    buildToDo(toDo);
+    newTaskForm.reset();
+  })
 });
+
+  function buildToDo(newToDo){
+    const task = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    deleteButton.textContent = 'x';
+    task.textContent = newToDo;
+    task.appendChild(deleteButton);
+    document.querySelector('#tasks').appendChild(task);
+  
+    deleteButton.addEventListener('click', handleDelete);
+  }
+
+  function handleDelete(e){
+    e.target.parentNode.remove();
+  }
